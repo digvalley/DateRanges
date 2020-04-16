4fb4b95dbc5e64846d0acf22134f22632baeb158
Thomas <Thomas@gravdal.co>
2020-04-16 01:36:21 +0100

    minimized the main function

--- index.js
+++ index.js
@@ -28,22 +28,19 @@ const findDateRanges = (dates = []) => {
     .map((x) => new Date(hasObjects ? x.date : x).getTime())
     .sort((a, b) => a - b)
 
-  // This is still a quick and VERY dirty string comp.
-  // but it works for now
   return sorted
     .reduce((acc, cur, i, v) => {
       let next = v[i + 1]
       let prev = v[i - 1]
 
-      if (!dayDiff(next, cur, 1) && !dayDiff(prev, cur, 1))
-        return `${acc}|${cur}|`
-      if (!dayDiff(next, cur, 1)) return `${acc}..${cur}`
-      if (!dayDiff(prev, cur, 1)) return `${acc}|${cur}..`
+      if (dayDiff(prev, cur, 1) && dayDiff(next, cur, 1)) return acc
+      if (dayDiff(prev, cur, 1) && !dayDiff(next, cur, 1))
+        acc[acc.length - 1].push(cur)
+      if (!dayDiff(prev, cur, 1)) return [...acc, [cur]]
+
       return acc
-    }, '')
-    .split('|')
-    .filter((x) => x !== '')
-    .map((x) => x.split('....').map((y) => new Date(+y)))
+    }, [])
+    .map((x) => x.map((y) => new Date(+y)))
 }
 
 module.exports = findDateRanges
